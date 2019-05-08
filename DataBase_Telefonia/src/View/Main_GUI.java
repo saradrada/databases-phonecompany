856a0b0aa027797ace1b5d5b88c2eba5a6e8c019
david1999erazo <erazo29@hotmail.com>
2019-05-07 23:13:30 -0500

    Add Login GUI

--- DataBase_Telefonia/src/View/Main_GUI.java
+++ DataBase_Telefonia/src/View/Main_GUI.java
@@ -1,5 +1,19 @@
 package View;
 
-public class Main_GUI {
+public class Main_GUI  {
 
+	
+	public Main_GUI() {
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Login_GUI login = new Login_GUI();
+		login.setVisible(true);
+		
+		
+	}
+	
+	
 }
