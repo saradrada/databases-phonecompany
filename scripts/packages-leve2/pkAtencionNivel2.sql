7e6689fef92edc823e880680120dad210bd967cf
Nicolas Biojo Bermeo <1107527983@academico.icesi.edu.co>
2019-05-25 15:35:35 -0500

    fix level two

--- scripts/packages-leve2/pkAtencionNivel2.sql
+++ scripts/packages-leve2/pkAtencionNivel2.sql
@@ -1,31 +1,39 @@
 CREATE OR REPLACE PACKAGE pkAtention AS
 
-PROCEDURE pAtencion ( ivCedulaFuncionario VARCHAR2, ivNumeroSolicitud VARCHAR2)
-FUNCTION fValidarSolicitudFuncionario (ivCedulaFuncionario VARCHAR2 , ivNumeroSolicitud VARCHAR2) return BOOLEAN
+PROCEDURE pAtencion ( ivCedulaFuncionario VARCHAR2, ivNumeroSolicitud VARCHAR2);
+FUNCTION fValidarSolicitudFuncionario (ivCedulaFuncionario VARCHAR2 , ivNumeroSolicitud VARCHAR2) return BOOLEAN;
 
 END pkAtention;/
 
 
 CREATE OR REPLACE PACKAGE BODY pkAtention AS
 
-FUNCTION  fValidarSolicitudFuncionario (ivCedulaFuncionario VARCHAR2 , ivNumeroSolicitud VARCHAR2) return  VARCHAR2
+FUNCTION  fValidarSolicitudFuncionario (ivCedulaFuncionario VARCHAR2 , ivNumeroSolicitud VARCHAR2) return  BOOLEAN
 IS
 	ovValidacion BOOLEAN; 
 	vFuncionario VARCHAR2 (50);
 	
 BEGIN
-	SELECT s.cedulaFuncionario INTO vFuncionario
-	FROM Solicitud s
-	WHERE s.num
-END
 
+    IF EXISTS(
+        SELECT *
+        FROM Asignacion 
+        WHERE Solicitud_numeroSolicitud = ivNumeroSolicitud AND Funcionario_cedula = ivCedulaFuncionario
+    ) 
+    THEN 
+        return TRUE;
+    ELSE
+        return FALSE;
+    END IF;
+
+END fValidarSolicitudFuncinario;
 
 PROCEDURE pAtencion ( ivCedulaFuncionario VARCHAR2, ivNumeroSolicitud VARCHAR2)
 IS
 BEGIN
 	
 
-END
+END 
 
 
 
--- scripts/packages-leve2/pkRegistroNivel2.sql
+++ scripts/packages-leve2/pkRegistroNivel2.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE PACKAGE pkRegistro AS
 
-PROCEDURE pRegistrarSolicitudCreacion (ivCedulaCliente VARCHAR2,ivObservacion VARCHAR2,ivTipoProducto VARCHAR2)
-PROCEDURE pRegistrarSolicitudModificacion (ivProductoCodigo VARCHAR2, ivCedulaCliente VARCHAR2, ivObservacion VARCHAR2, ivTipoProducto VARCHAR2 )
-PROCEDURE pRegistrarSolicitudCancelacion ( ivProductoCodigo VARCHAR2 , ivCedulaCliente VARCHAR2 , ivObservacion VARCHAR2)
-PROCEDURE pRegistrarSolicitudReclamo ( ivProductoCodigo VARCHAR2 , ivCedulaCliente VARCHAR2, ivObservacion VARCHAR2)
-FUNCTION fConsultarNumLibre () return  VARCHAR2
+PROCEDURE pRegistrarSolicitudCreacion (ivProductoCodigo VARCHAR2, ivCedulaCliente VARCHAR2,ivObservacion VARCHAR2,ivTipoProducto VARCHAR2);
+PROCEDURE pRegistrarSolicitudModificacion (ivProductoCodigo VARCHAR2, ivCedulaCliente VARCHAR2, ivObservacion VARCHAR2, ivTipoProducto VARCHAR2 );
+PROCEDURE pRegistrarSolicitudCancelacion ( ivProductoCodigo VARCHAR2 , ivCedulaCliente VARCHAR2 , ivObservacion VARCHAR2);
+PROCEDURE pRegistrarSolicitudReclamo ( ivProductoCodigo VARCHAR2 , ivCedulaCliente VARCHAR2, ivObservacion VARCHAR2);
+FUNCTION fConsultarNumLibre return  VARCHAR2;
 
 END pkRegistro;/
 
@@ -12,58 +12,58 @@ END pkRegistro;/
 
 CREATE OR REPLACE PACKAGE BODY pkRegistro AS
 
-FUNCTION  fConsultarNumLibre () return  VARCHAR2
+FUNCTION  fConsultarNumLibre return  VARCHAR2
 IS
 	ovNumero VARCHAR2(50);
 	vUltimaSolicitud VARCHAR2(50);	
 BEGIN
 	SELECT MAX(numSolicitud) INTO vUltimaSolicitud
-	FROM Solicitud
-	CONVERT(int,vUltimaSolicitud)
+	FROM Solicitud;
+	CONVERT(int,vUltimaSolicitud);
 	--Verificar que este convert funcione solamente llamandolo
 	ovNumero := vUltimaSolicitud+1; 
 	return ovNumero;
-END
+END fConsultarNumLibre;
 
 
 
 --VERIFICAR COMO HACER LA CREACION 
-PROCEDURE pRegistrarSolicitudCreacion (ivCedulaCliente VARCHAR2, ivObservacion VARCHAR2, ivTipoProducto VARCHAR2)
+PROCEDURE pRegistrarSolicitudCreacion (ivProductoCodigo VARCHAR2,ivCedulaCliente VARCHAR2, ivObservacion VARCHAR2, ivTipoProducto VARCHAR2)
 IS
 	vNumero VARCHAR2(50);
-	vNumero := fConsultarNumLibre();
 BEGIN
-	pkSolicitudNivel1.pInsertarSolicitud( vNumero,ivObservacion, 'Nuevo' ,ivProductoCodigo VARCHAR2, ivCedulaCliente , 'A0') -- Tipo de registro, quemado en las tablas
-END
+    vNumero := fConsultarNumLibre;
+	pkSolicitudNivel1.pInsertarSolicitud( vNumero,ivObservacion, 'Nuevo' ,ivProductoCodigo, ivCedulaCliente , 'A0'); -- Tipo de registro, quemado en las tablas
+END pRegistrarSolicitudCreacion;
 
 
 
 PROCEDURE pRegistrarSolicitudModificacion (ivProductoCodigo VARCHAR2, ivCedulaCliente VARCHAR2, ivObservacion VARCHAR2, ivTipoProducto VARCHAR2 )
 IS
 	vNumero VARCHAR2(50);
-	vNumero := fConsultarNumLibre();
 BEGIN
-	pkSolicitudNivel1.pInsertarSolicitud( vNumero, ivObservacion, 'Nuevo' ,ivProductoCodigo, ivCedulaCliente , 'B1') -- Tipo de registro, quemado en las tablas
-END
+    vNumero := fConsultarNumLibre();
+	pkSolicitudNivel1.pInsertarSolicitud( vNumero, ivObservacion, 'Nuevo' ,ivProductoCodigo, ivCedulaCliente , 'B1'); -- Tipo de registro, quemado en las tablas
+END pRegistrarSolicitudModificacion;
 
 
 
 PROCEDURE pRegistrarSolicitudCancelacion ( ivProductoCodigo VARCHAR2 , ivCedulaCliente VARCHAR2 , ivObservacion VARCHAR2)
 IS
 	vNumero VARCHAR2(50);
-	vNumero := fConsultarNumLibre();	
 BEGIN
-	pkSolicitudNivel1.pInsertarSolicitud( vNumero, ivObservacion, 'Nuevo' ,ivProductoCodigo, ivCedulaCliente , 'C2') -- Tipo de registro, quemado en las tablas
-END
+    vNumero := fConsultarNumLibre();	
+	pkSolicitudNivel1.pInsertarSolicitud( vNumero, ivObservacion, 'Nuevo' ,ivProductoCodigo, ivCedulaCliente , 'C2'); -- Tipo de registro, quemado en las tablas
+END pRegistrarSolicitudCancelacion;
 
 
 
 PROCEDURE pRegistrarSolicitudReclamo ( ivProductoCodigo VARCHAR2 , ivCedulaCliente VARCHAR2, ivObservacion VARCHAR2)
 IS
 	vNumero VARCHAR2(50);
-	vNumero := fConsultarNumLibre();
 BEGIN
-	pkSolicitudNivel1.pInsertarSolicitud( vNumero, ivObservacion, 'Nuevo' ,ivProductoCodigo, ivCedulaCliente , 'D3') -- Tipo de registro, quemado en las tablas
-END
+    vNumero := fConsultarNumLibre();
+	pkSolicitudNivel1.pInsertarSolicitud( vNumero, ivObservacion, 'Nuevo' ,ivProductoCodigo, ivCedulaCliente , 'D3'); -- Tipo de registro, quemado en las tablas
+END pRegistrarSolicitudReclamo;
 
 END pkRegistro;
