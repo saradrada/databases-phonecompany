3fb352cc2d15e4cd533641ff17aedf06ecf94fa5
Nicolás Biojo Bermeo <nicolas.biojo@correo.icesi.edu.co>
2019-05-26 21:07:39 -0500

    Update CannectioDB

--- src/model/Client.java
+++ src/model/Client.java
@@ -3,30 +3,20 @@ package model;
 import java.sql.Connection;
 
 public class Client {
-	
-	public static void AddClient(Connection connection, String ivId,String ivContrasenha,String ivNombre ,String ivCedula ,String ivDireccion,String  ivFecha, String ivTelefono) throws Exception {
-		try {
-			String Statament = "BEGIN "; 
-			Statament += "pkRegistro.pRegistrarCliente(";
-			Statament += "'"+ivId + "','" + ivContrasenha + "','" + ivNombre + "','" +ivCedula + "','" + ivDireccion + "','" +  ivFecha + "','" +  ivTelefono+"');";
-			Statament += " END;";
+
+	public static void AddClient(Connection connection, String ivId, String ivContrasenha, String ivNombre,
+			String ivCedula, String ivDireccion, String ivFecha, String ivTelefono) throws Exception {
+		
+		String Statament = "pkRegistro.pRegistrarCliente('" + ivId + "','" + ivContrasenha + "','" + ivNombre + "','"
+				+ ivCedula + "','" + ivDireccion + "','" + ivFecha + "','" + ivTelefono + "');";
+		
+		ConnectionDB.send(connection, Statament);
+	}
+
+	public static void deletesCLient(Connection connection, String ivCedula) throws Exception {
+			String Statament =  "pkEliminar.pEliminarCliente('" + ivCedula + "');";
 			ConnectionDB.send(connection, Statament);
-			System.out.println("Se agrego un registro");
-		} catch (Exception e) {
-			throw new Exception(e.getMessage().split("\n")[0].split(":")[2]);
-		}
 	}
 	
-	public static void deletesCLient(Connection connection,String ivCedula) throws Exception {
-		try {
-			String Statament = "BEGIN "; 
-			Statament += "pkEliminar.pEliminarCliente(";
-			Statament += "'" +ivCedula + "');";
-			Statament += " END;";
-			ConnectionDB.send(connection, Statament);
-			System.out.println("Se elimino el registro");
-		} catch (Exception e) {
-			throw new Exception(e.getMessage().split("\n")[0].split(":")[1]);
-		}
-	}
+	
 }
